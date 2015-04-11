5e9116e8ea6a9f6cb0ffbba9d93e95c306d803aa
lillisgary <lillisgary@gmail.com>
2015-04-11 14:12:09 +0200

    Adding the Slide model to models and admin

--- theme/admin.py
+++ theme/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from mezzanine.pages.admin import PageAdmin
-from .models import HomePage
+from .models import HomePage, Slide
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+
+class SlideInline(TabularDynamicInlineAdmin):
+    model = Slide
 
 class HomePageAdmin(PageAdmin):
-	pass
+	inlines = (SlideInline,)
 	
 admin.site.register(HomePage, HomePageAdmin)
--- theme/models.py
+++ theme/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from mezzanine.core.fields import FileField
 from mezzanine.pages.models import Page
-from mezzanine.core.models import RichText
+from mezzanine.core.models import RichText, Orderable
+from mezzanine.utils.models import upload_to
 
 class HomePage(Page, RichText):
+	
 	
 	class Meta:
 		verbose_name = _("Home page")
 		verbose_name_plural = _("Home pages")
+
+class Slide(Orderable):
+    '''
+    A slide in a slider connected to a HomePage
+    '''
+    homepage = models.ForeignKey(HomePage, related_name="slides")
+    image = FileField(verbose_name=_("Image"),
+        upload_to=upload_to("theme.Slide.image", "slider"),
+        format="Image", max_length=255, null=True, blank=True)
