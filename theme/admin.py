e3ecca30eb5dc6635161ad7adb620b0b96f52595
lillisgary <lillisgary@gmail.com>
2015-05-17 13:06:17 +0200

    updated the admin.py for the new models

--- theme/admin.py
+++ theme/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from mezzanine.pages.admin import PageAdmin
-from .models import HomePage, Slide
+from .models import HomePage, Slide, Portfolio, PortfolioItem, PortfolioItemImage, PortfolioItemCategory
 from mezzanine.core.admin import TabularDynamicInlineAdmin
 
 class SlideInline(TabularDynamicInlineAdmin):
@@ -9,5 +9,14 @@ class SlideInline(TabularDynamicInlineAdmin):
 
 class HomePageAdmin(PageAdmin):
 	inlines = (SlideInline,)
+
+class PortfolioItemImageInline(TabularDynamicInlineAdmin):
+    model = PortfolioItemImage
+
+class PortfolioItemAdmin(PageAdmin):
+    inlines = (PortfolioItemImageInline,)
 	
 admin.site.register(HomePage, HomePageAdmin)
+admin.site.register(Portfolio, PageAdmin)
+admin.site.register(PortfolioItemCategory)
+admin.site.register(PortfolioItem, PortfolioItemAdmin)
